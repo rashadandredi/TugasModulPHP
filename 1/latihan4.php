700857ae7b8e2d13227fdb24c709d426a85f3a4f
Rashad <46430370+rashadandredi@users.noreply.github.com>
2019-02-20 13:49:25 +0700

    selesai dikerjakan

---
+++ 1/latihan4.php
@@ -0,0 +1,8 @@
+<?php
+  $nama = "Rashad Izza";
+  function tampil_nama(){
+      global $nama;
+      echo "Nama saya : ".$nama;
+  }
+  tampil_nama();
+?>
---
+++ 1/latihan5.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>variabel antar Halaman</title>
+  </head>
+  <body>
+    <form action="variabel_hasil.php" method="post">
+      Nama : <input type="text" name="nama"> <br>
+      Email : <input type="text" name="email"> <br>
+      <input type="submit" name="submit" value="Tampilkan">
+    </form>
+  </body>
+</html>
